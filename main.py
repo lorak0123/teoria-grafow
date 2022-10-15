408cd3e5c3da59681f39d4a15af85cf9c9489bc8
Artur Leśnik <artur.lesnik@edu.uekat.pl>
2022-10-15 15:25:47 +0200

    dodano: modularyzację procedury wyświetlania mapy w projekcie

--- main.py
+++ main.py
@@ -1,21 +1,4 @@
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-
-
-RESOURCE_POLAND_MAP_PATH: str = "resources/images/poland-map.jpg"
-
-
-def show_poland_map() -> None:
-    """
-    Funkcja powoduje pokazanie na ekranie kompletnej mapy Polski bez dodatkowych oznaczeń.
-
-    :return: None
-    """
-    poland_map = mpimg.imread(RESOURCE_POLAND_MAP_PATH, "jpg")
-    plt.figure(figsize=(30, 30))
-    plt.imshow(poland_map)
-    plt.show()
-    return None
+from controllers.poland_map_controller import show_poland_map
 
 
 show_poland_map()
