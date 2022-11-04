1539b357c5eee349dde4c131359e646d1e154ce3
Artur Leśnik <artur.lesnik@edu.uekat.pl>
2022-11-04 10:06:54 +0100

    dodano: stałe potrzebne do wykonania obliczeń na mapie Polski

--- config/constants.py
+++ config/constants.py
@@ -1,3 +1,16 @@
 RESOURCE_POLAND_MAP_PATH: str = "resources/images/poland-map.jpg"
 """Zawiera lokalizację mapy Polski w projekcie"""
 RESOURCE_POLAND_CITIES: str = "resources/data/miasta.xlsx"
+"""Zawiera lokalizację pliku z miastami Polski i ich umiejscowieniem"""
+MAP_GPS_LONG_MIN: float = 49.00
+"""Zawiera minimalną szerokość geograficzną obszaru Polski"""
+MAP_GPS_LONG_MAX: float = 54.50
+"""Zawiera maksymalną szerokość geograficzną obszaru Polski"""
+MAP_GPS_LANG_MIN: float = 14.07
+"""Zawiera minimalną wysokość geograficzną obszaru Polski"""
+MAP_GPS_LANG_MAX: float = 24.09
+"""Zawiera maksymalną szerokość geograficzną obszaru Polski"""
+MAP_PX_WIDTH_SIZE: int = 1400
+"""Szerokość obrazu zawierającego mapę Polski"""
+MAP_PX_HEIGHT_SIZE: int = 1400
+"""Wysokość obrazu zawierajacego mapę Polski"""
