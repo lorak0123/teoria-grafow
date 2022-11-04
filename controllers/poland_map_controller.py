f792e6868203a1789b43e6c5b1e6dbb1481ab1a7
Artur Leśnik <artur.lesnik@edu.uekat.pl>
2022-11-04 09:45:03 +0100

    dodano: przekazanie wylosowanych miast do narysowania mapy

--- controllers/poland_map_controller.py
+++ controllers/poland_map_controller.py
@@ -2,9 +2,10 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 from config.constants import RESOURCE_POLAND_MAP_PATH
+from models.City import City
 
 
-def show_poland_map() -> None:
+def show_poland_map(cities: list[City]) -> None:
     """
     Funkcja powoduje pokazanie na ekranie kompletnej mapy Polski bez dodatkowych oznaczeń.
 
