145cf7fce4deac313015edfb31e777f4097a411f
Karol Pilot <pilot.karol.lh@gmail.com>
2022-11-23 18:59:56 +0100

    Added optimization

--- models/City.py
+++ models/City.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
+from math import radians, cos, sin, asin, sqrt
+
 
 class City:
     """
     Klasa zawierająca informacje o miastach, które zostały wylosowane do projektu budowy szybkich dróg.
     """
+
     def __init__(self, name: str, latitude: str, longitude: str) -> None:
         self.name: str = name
         self.latitude: str = latitude
@@ -17,9 +20,9 @@ class City:
 
         :return: str
         """
-        return f"City: { self.name }, latitude: { self.latitude }, longitude: { self.longitude }"
+        return f"City: {self.name}, latitude: {self.latitude}, longitude: {self.longitude}"
 
-    def add_new_connection(self, add_city: City) -> None:
+    def add_new_connection(self, add_city: City) -> float:
         """
         Dodaje do miasta informację o połączonych z nim innymi miastami.
 
@@ -28,7 +31,8 @@ class City:
         """
         if self.name is not add_city.name:
             self.__connected_cities.append(add_city)
-        return None
+            return self.calculate_distance(add_city)
+        return 0
 
     def get_city_connections(self) -> list[City]:
         """
@@ -37,3 +41,26 @@ class City:
         :return: list[City]
         """
         return self.__connected_cities
+
+    def calculate_distance(self, other: City) -> float:
+        """
+        Funkcja zwraca odległość miasta od drugiego
+
+        :return: float
+        """
+        lat1 = float(self.latitude.replace(" °N", ""))
+        lon1 = float(self.longitude.replace(" °E", ""))
+        lat2 = float(other.latitude.replace(" °N", ""))
+        lon2 = float(other.longitude.replace(" °E", ""))
+
+        dlon = lon2 - lon1
+        dlat = lat2 - lat1
+        a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+
+        c = 2 * asin(sqrt(a))
+
+        # Radius of earth in kilometers. Use 3956 for miles
+        r = 6371
+
+        # calculate the result
+        return (c * r)
